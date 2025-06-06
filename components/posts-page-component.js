ac029f434951c65fc977bf4d4f1598c6e2e5291a
John Doe <johndoe@email.com>
2025-06-06 17:45:49 +0500

    1

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -6,9 +6,6 @@ export function renderPostsPageComponent({ appEl }) {
   // @TODO: реализовать рендер постов из api
   console.log("Актуальный список постов:", posts);
 
-
-
-  
   /**
    * @TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
    * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
